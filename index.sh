0a397a8d8b8f078ba0602293935fd07397f126c6
Ayu <128837235+ayulume@users.noreply.github.com>
2026-05-08 23:28:14 +0200

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,42 @@
+echo ""
+echo ""
+echo "  █                   █                       
+ █ █ █   █ █  █       █    █  █  ██ ██   ████ 
+ █ █ █   █ █  █       █    █  █ █  █  █ █    █
+█   █ █ █  █  █       █    █  █ █  █  █ █████ 
+█████  █   █  █       █    █  █ █     █ █     
+█   █  █    ██        ████  ██  █     █  █████
+       █                                      
+     ██                                       "
+
+echo "ayulume.is-a.dev"
+echo ""
+echo ""
+echo ""
+
+echo "Latest blog entry"
+echo "Coming back soon!"
+
+echo ""
+echo ""
+echo ""
+
+echo "
+┌──────Projects──────┬───────────────────┐
+│                    │                   │
+│ Seqyusphere        │ seqyusphere.eu    │
+│ YouTube (French)   │ @ayulumevf        │
+│ Bluesky            │ @seqyusphere.eu   │
+│ Twitter            │ @ayulume          │
+│                    │                   │
+└────────────────────┴───────────────────┘"
+
+echo "
+┌────Social Media────┬───────────────────┐
+│                    │                   │
+│ YouTube (English)  │ @ayulume          │
+│ YouTube (French)   │ @ayulumevf        │
+│ Bluesky            │ @seqyusphere.eu   │
+│ Twitter            │ @ayulume          │
+│                    │                   │
+└────────────────────┴───────────────────┘"
